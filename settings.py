d3dd47b4bf5b419e40a3e5e6f685293f65d438df
Daniele Basile <asterix24@gmail.com>
2012-02-19 15:23:32 +0100

    Remove db from rep and add main app.

--- settings.py
+++ settings.py
@@ -116,11 +116,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'GestionaleCaldaie'
+    'main',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    #'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    #'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
