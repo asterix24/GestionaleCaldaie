86304799c7ae8fa8a40a81424394ebcf35c4077b
Daniele Basile <asterix24@gmail.com>
2012-02-19 15:23:08 +0100

    Start to define the database model.

--- main/models.py
+++ main/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Cliente(models.Model):
+    codice_id = models.IntegerField()
+    cognome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=100)
+    codice_fiscale = models.CharField(max_length=17)
+    via = models.TextField()
+    numero_civico = models.CharField(max_length=10)
+    citta = models.TextField()
+    numero_telefono = models.IntegerField()
+    marca_caldaia = models.CharField(max_length=100)
+    modello_caldaia = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=1)
+    combustibile = models.CharField(max_length=100)
+    data_installazione = models.DateField()
+
+
+    def __unicode__(self):
+        return self.nome
+
+
+
