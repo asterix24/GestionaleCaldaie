b54a3e2632b9a827cea3b28907f4f800481620ce
Daniele Basile <asterix24@gmail.com>
2012-04-13 00:15:35 +0200

    Other tests.

--- main/views.py
+++ main/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import render_to_response
 
 def test(req):
     filtro = req.GET.get('q', '')
-    return render_to_response('test.html', {'clienti': clienti.filter_records(models.Cliente.objects, "cognome", filtro)})
+    print req.GET.keys()
+    print req.GET.values()
+    return render_to_response('test.html',
+        {'clienti': clienti.filter_records(models.Cliente.objects, "cognome", filtro)}
+        )
 
 
