10b5edfd4bd19d34dbea203407e07fa945e86d3b
Daniele Basile <asterix@develer.com>
2012-05-11 18:42:56 +0200

    Start the upgrade to django 1.4.

--- main/admin.py
+++
@@ -1,8 +0,0 @@
-from main.models import Cliente
-from main.models import Bollino
-from main.models import Intervento
-from django.contrib import admin
-
-admin.site.register(Cliente)
-admin.site.register(Bollino)
-admin.site.register(Intervento)
