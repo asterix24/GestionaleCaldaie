6fe2bfaba53319a0915ca93668dc6f52cc11ae81
Daniele Basile <asterix@develer.com>
2012-10-18 12:36:04 +0000

    Reformat and remove dev link.

--- gestionale/urls.py
+++ gestionale/urls.py
@@ -5,43 +5,43 @@ from django.conf.urls.defaults import patterns, include, url
 #admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'main.views.home', name='home'),
-    url(r'^home/$', 'main.views.home', name='home'),
+	url(r'^$', 'main.views.home', name='home'),
+	url(r'^home/$', 'main.views.home', name='home'),
 
-    url(r'^anagrafe/$', 'main.views.anagrafe', name='anagrafe'),
+	url(r'^anagrafe/$', 'main.views.anagrafe', name='anagrafe'),
 
-    # Client manager urls
-    url(r'^anagrafe/add/$', 'main.views.add_record', name='add_record'),
-    url(r'^anagrafe/(?P<cliente_id>\d+)/$', 'main.views.detail_record', name='detail_record'),
-    url(r'^anagrafe/(?P<cliente_id>\d+)/edit/$', 'main.views.edit_record', name='edit_record'),
-    url(r'^anagrafe/(?P<cliente_id>\d+)/delete/$', 'main.views.delete_record', name='delete_record'),
+	# Client manager urls
+	url(r'^anagrafe/add/$', 'main.views.add_record', name='add_record'),
+	url(r'^anagrafe/(?P<cliente_id>\d+)/$', 'main.views.detail_record', name='detail_record'),
+	url(r'^anagrafe/(?P<cliente_id>\d+)/edit/$', 'main.views.edit_record', name='edit_record'),
+	url(r'^anagrafe/(?P<cliente_id>\d+)/delete/$', 'main.views.delete_record', name='delete_record'),
 
-    # Impianti manager urls
+	# Impianti manager urls
 	url(r'^anagrafe/(?P<cliente_id>\d+)/(?P<detail_type>\w+)/add/$', 'main.views.add_record', name='add_record'),
-    # Add new Impianto to existing Cliente
-    url(r'^anagrafe/(?P<cliente_id>\d+)/(?P<detail_type>\w+)/(?P<impianto_id>\d+)/add/$', 'main.views.add_record', name='add_record'),
-    # Add new Verifiche/Interventi to existing Impianto
-    url(r'^anagrafe/(?P<cliente_id>\d+)/impianto/(?P<impianto_id>\d+)/(?P<detail_type>\w+)/add/$', 'main.views.add_record', name='add_record'),
+	# Add new Impianto to existing Cliente
+	url(r'^anagrafe/(?P<cliente_id>\d+)/(?P<detail_type>\w+)/(?P<impianto_id>\d+)/add/$', 'main.views.add_record', name='add_record'),
+	# Add new Verifiche/Interventi to existing Impianto
+	url(r'^anagrafe/(?P<cliente_id>\d+)/impianto/(?P<impianto_id>\d+)/(?P<detail_type>\w+)/add/$', 'main.views.add_record', name='add_record'),
 
-    # Delete Impianti
-    url(r'^anagrafe/(?P<cliente_id>\d+)/(?P<detail_type>\w+)/(?P<impianto_id>\d+)/delete/$', 'main.views.delete_record', name='delete_record'),
-    # Delete verifiche/interventi
-    #/anagrafe/356/impianto/762/verifiche/758/delete/
+	# Delete Impianti
+	url(r'^anagrafe/(?P<cliente_id>\d+)/(?P<detail_type>\w+)/(?P<impianto_id>\d+)/delete/$', 'main.views.delete_record', name='delete_record'),
+	# Delete verifiche/interventi
+	#/anagrafe/356/impianto/762/verifiche/758/delete/
 	url(r'^anagrafe/(?P<cliente_id>\d+)/impianto/(?P<impianto_id>\d+)/(?P<detail_type>\w+)/(?P<sub_impianto_id>\d+)/delete/$', 'main.views.delete_record', name='delete_record'),
-    # Show Impianti detail of select Cliente
+	# Show Impianti detail of select Cliente
 	url(r'^anagrafe/(?P<cliente_id>\d+)/(?P<detail_type>\w+)/(?P<impianto_id>\d+)/$', 'main.views.detail_record', name='detail_record'),
-    # Show Verifiche/Interventi detail of select Impianti
+	# Show Verifiche/Interventi detail of select Impianti
 	url(r'^anagrafe/(?P<cliente_id>\d+)/impianto/(?P<impianto_id>\d+)/(?P<detail_type>\w+)/(?P<sub_impianto_id>\d+)/$', 'main.views.detail_record', name='detail_record'),
 
-    # /anagrafe/356/impianto/760/edit/
+	# /anagrafe/356/impianto/760/edit/
 	url(r'^anagrafe/(?P<cliente_id>\d+)/(?P<detail_type>\w+)/(?P<impianto_id>\d+)/edit/$', 'main.views.edit_record', name='edit_record'),
-    # /anagrafe/356/impianto/760/verifiche/1/edit/
+	# /anagrafe/356/impianto/760/verifiche/1/edit/
 	url(r'^anagrafe/(?P<cliente_id>\d+)/impianto/(?P<impianto_id>\d+)/(?P<detail_type>\w+)/(?P<sub_impianto_id>\d+)/edit/$', 'main.views.edit_record', name='edit_record'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    # Uncomment the next line to enable the admin:
-    #url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the admin/doc line below to enable admin documentation:
+	#url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the next line to enable the admin:
+	#url(r'^admin/', include(admin.site.urls)),
 
-	url(r'^dev/populatedb/$', 'main.views.populatedb', name='populat_db'),
+	#url(r'^dev/populatedb/$', 'main.views.populatedb', name='populat_db'),
 )
