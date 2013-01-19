32239cf4bbe9c4ce11ae9b492abbd6c867a7b81a
Daniele Basile <asterix24@gmail.com>
2013-01-19 16:22:50 +0100

    Clean up.

--- main/database_manager.py
+++ main/database_manager.py
@@ -16,47 +16,6 @@ from tools import *
 import logging
 logger = logging.getLogger(__name__)
 
-FILTER_MODE_START =    '__istartswith'
-FILTER_MODE_EXACT =    '__iexact'
-FILTER_MODE_CONTAIN =  '__icontains'
-FILTER_SHORT_ASCEN =   0
-FILTER_SHORT_DESCEND = 1
-
-def filter_records(ctx, key, value, mode = FILTER_MODE_CONTAIN):
-    return ctx.filter(**{ key + mode : value })
-
-def filter_dataInstallazione(ctx, start_y, start_m = None, start_d = None, stop_y = None, stop_m = None, stop_d = None):
-    if stop_y is not None and stop_m is not None and stop_d is not None:
-            return ctx.filter(data_installazione__range = (datetime.date(start_y, start_m, start_d),datetime.date(stop_y, stop_m, stop_d)))
-    if start_m is not None:
-            return ctx.filter(data_installazione__year = start_y).filter(data_installazione__month = start_m)
-    else:
-            return ctx.filter(data_installazione__year = start_y)
-
-def filter_dataContratto(ctx, start_y, start_m = None, start_d = None, stop_y = None, stop_m = None, stop_d = None):
-    if stop_y is not None and stop_m is not None and stop_d is not None:
-            return ctx.filter(data_contratto__range = (datetime.date(start_y, start_m, start_d),datetime.date(stop_y, stop_m, stop_d)))
-    if start_m is not None:
-            return ctx.filter(data_contratto__year = start_y).filter(data_installazione__month = start_m)
-    else:
-            return ctx.filter(data_contratto__year = start_y)
-
-def clienti_displayAll(ctx):
-    return ctx.all()
-
-def select_record(ctx, id):
-    return ctx.get(pk=id)
-
-def delete_record(ctx, id):
-    return ctx.objects.get(pk=id).delete()
-
-def insert_cliente(r):
-    node = Cliente(**r)
-#       dump(r)
-    node.save()
-    return node
-
-
 #SELECT
 DB_COLUM = """
 main_cliente.numero_cellulare,
