d61198ed3bd51f9f97aab6b002c101d63738b6d2
Daniele Basile <asterix@develer.com>
2013-01-29 14:16:39 +0000

    Remove test and dev url from public.

--- gestionale/urls.py
+++ gestionale/urls.py
@@ -43,6 +43,6 @@ urlpatterns = patterns('',
 	# Uncomment the next line to enable the admin:
 	#url(r'^admin/', include(admin.site.urls)),
 
-	url(r'^dev/populatedb/$', 'main.views.populatedb', name='populat_db'),
-	url(r'^test/(?P<search_string>\w+)/$', 'main.views.test', name='test'),
+	#url(r'^dev/populatedb/$', 'main.views.populatedb', name='populat_db'),
+	#url(r'^test/(?P<search_string>\w+)/$', 'main.views.test', name='test'),
 )
