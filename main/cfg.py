38ff79aa6d52efe19b0c727bb95655e09c5c90d3
Daniele Basile <asterix24@gmail.com>
2013-04-24 00:02:50 +0200

    Add colum ordering for all field.

--- main/cfg.py
+++ main/cfg.py
@@ -104,6 +104,17 @@ ANAGRAFE_INTERVENTI_STD_VIEW = [
 'note_intervento'
 ]
 
+DB_DATA_FIELD = [
+'data_verifica',
+'data_installazione',
+'data_contratto',
+'prossima_verifica',
+'prossima_analisi_combustione',
+'data_ultima_verifica',
+'ultima_analisi_combustione',
+'prossima_analisi_combustione',
+]
+
 GROUP_FIELD_VIEW = {
 'codice_impianto'     : { "field":"main_cliente.codice_impianto",       "order":"asc" },
 'cognome'             : { "field":"main_cliente.cognome",               "order":"asc" },
@@ -115,6 +126,7 @@ GROUP_FIELD_VIEW = {
 'numero_cellulare'    : { "field":"main_cliente.numero_cellulare",      "order":"asc" },
 'numero_telefono'     : { "field":"main_cliente.numero_telefono",       "order":"asc" },
 'mail'                : { "field":"main_cliente.mail",                  "order":"asc" },
+
 'stato_impianto'      : { "field":"main_impianto.stato_impianto",       "order":"asc" },
 'marca_caldaia'       : { "field":"main_impianto.marca_caldaia",        "order":"asc" },
 'modello_caldaia'     : { "field":"main_impianto.modello_caldaia",      "order":"asc" },
@@ -125,8 +137,31 @@ GROUP_FIELD_VIEW = {
 'data_installazione'  : { "field":"main_impianto.data_installazione",   "order":"asc" },
 'data_contratto'      : { "field":"main_impianto.data_contratto",       "order":"asc" },
 'anzianita_impianto'  : { "field":"main_impianto.anzianita_impianto",   "order":"asc" },
-}
 
+'stato_verifica'                : { "field":"main_verifica.stato_verifica",                "order":"asc"},
+'data_verifica'                 : { "field":"main_verifica.data_verifica",                 "order":"asc"},
+'tipo_verifica'                 : { "field":"main_verifica.tipo_verifica",                 "order":"asc"},
+'altro_tipo_verifica'           : { "field":"main_verifica.altro_tipo_verifica",           "order":"asc"},
+'codice_id'                     : { "field":"main_verifica.codice_id",                     "order":"asc"},
+'numero_rapporto'               : { "field":"main_verifica.numero_rapporto",               "order":"asc"},
+'prossima_verifica'             : { "field":"main_verifica.prossima_verifica",             "order":"asc"},
+'analisi_combustione'           : { "field":"main_verifica.analisi_combustione",           "order":"asc"},
+'colore_bollino'                : { "field":"main_verifica.colore_bollino",                 "order":"asc"},
+'altro_colore_bollino'          : { "field":"main_verifica.altro_colore_bollino",           "order":"asc"},
+'numero_bollino'                : { "field":"main_verifica.numero_bollino",                 "order":"asc"},
+'valore_bollino'                : { "field":"main_verifica.valore_bollino",                 "order":"asc"},
+'prossima_analisi_combustione'  : { "field":"main_verifica.prossima_analisi_combustione",   "order":"asc"},
+'stato_pagamento'               : { "field":"main_verifica.stato_pagamento",                "order":"asc"},
+'costo_intervento'              : { "field":"main_verifica.costo_intervento",               "order":"asc"},
+'note_verifica'                 : { "field":"main_verifica.note_verifica",                  "order":"asc"},
+'data_ultima_verifica'          : { "field":"main_verifica.data_ultima_verifica",           "order":"asc"},
+'ultima_analisi_combustione'    : { "field":"main_verifica.ultima_analisi_combustione",     "order":"asc"},
+
+'data_intervento'               : { "field":"main_intervento.data_intervento",     "order":"asc"},
+'tipo_intervento'               : { "field":"main_intervento.tipo_intervento",     "order":"asc"},
+'note_intervento'               : { "field":"main_intervento.note_intervento",     "order":"asc"},
+
+}
 
 DATA_FIELD_STR_FORMAT = "%d/%m/%Y"
 
