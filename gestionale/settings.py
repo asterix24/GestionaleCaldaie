f8317eaaf0f1e0a79fe81ba164a294579b5feba0
Daniele Basile <asterix24@gmail.com>
2013-05-24 23:05:36 +0200

    Enable admin and and entry point for login page.

--- gestionale/settings.py
+++ gestionale/settings.py
@@ -106,9 +106,9 @@ INSTALLED_APPS = (
     'main',
     'south',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 import sys
