13667c31dcf2c4dac39777a85bcfa7bdebc1de4d
Daniele Basile <asterix@develer.com>
2013-05-30 15:45:04 +0200

    WIP

--- gestionale/admin.py
+++ gestionale/admin.py
@@ -1,4 +1,7 @@
-from main.models import Cliente
 from django.contrib import admin
+from main.models import Cliente, Impianto, Verifica, Intervento
 
 admin.site.register(Cliente)
+admin.site.register(Impianto)
+admin.site.register(Verifica)
+admin.site.register(Intervento)
