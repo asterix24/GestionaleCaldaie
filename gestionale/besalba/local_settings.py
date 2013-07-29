ad1b61d0116bff2d876279f0d2e2a06d50216dbd
Daniele Basile <asterix24@gmail.com>
2013-07-29 22:45:48 +0200

    Reorder directory settings.

--- gestionale/besalba/local_settings.py
+++ gestionale/besalba/local_settings.py
@@ -1,22 +1,5 @@
-"""
-ADMINS = (
-            # ('Your Name', 'your_email@example.com'),
-        )
+APP_PREFIX_NAME='besalba'
 
-MANAGERS = ADMINS
-
-DATABASES = {
-   'default': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ora
-            'NAME': 'database.db',           # Or path to database file if using sqlite3.
-            'USER': '',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }
-}
-
-"""
 ADMINS = (
     ('Daniele Basile', 'asterix24@gmail.com'),
 )
--- gestionale/demo/local_settings.py
+++ gestionale/demo/local_settings.py
@@ -1,22 +1,5 @@
-"""
-ADMINS = (
-            # ('Your Name', 'your_email@example.com'),
-        )
+APP_PREFIX_NAME='demo'
 
-MANAGERS = ADMINS
-
-DATABASES = {
-   'default': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ora
-            'NAME': 'database.db',           # Or path to database file if using sqlite3.
-            'USER': '',                      # Not used with sqlite3.
-            'PASSWORD': '',                  # Not used with sqlite3.
-            'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-        }
-}
-
-"""
 ADMINS = (
     ('Daniele Basile', 'asterix24@gmail.com'),
 )
@@ -26,7 +9,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'gestionale',           # Or path to database file if using sqlite3.
+        'NAME': 'demo-gestionale',           # Or path to database file if using sqlite3.
         'USER': 'asterix',                      # Not used with sqlite3.
         'PASSWORD': '1234567890',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
