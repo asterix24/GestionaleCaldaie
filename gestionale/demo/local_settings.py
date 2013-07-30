93ee63003e5e4e7e7695a0f0d19e289fd9da43bc
Daniele Basile <asterix24@gmail.com>
2013-07-30 23:52:32 +0200

    Other fix to use more that one settings.

--- gestionale/demo/local_settings.py
+++ gestionale/demo/local_settings.py
@@ -9,21 +9,23 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'demo-gestionale',           # Or path to database file if using sqlite3.
-        'USER': 'asterix',                      # Not used with sqlite3.
-        'PASSWORD': '1234567890',                  # Not used with sqlite3.
+        'NAME': 'demo_gestionale',           # Or path to database file if using sqlite3.
+        'USER': 'demo',                      # Not used with sqlite3.
+        'PASSWORD': '123456',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
-SITE_ROOT = "/tmp"
+
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'eq2u35)_qf#zrn$n97+o4belkhe9j=xq17yx75f79u*ma*y7wb'
-LOCAL_PATH = '/Users/asterix/src/sviluppo_e_altro/GestionaleCaldaie/'
+LOCAL_ROOT_PATH = '/Users/asterix/src/sviluppo_e_altro/GestionaleCaldaie/'
+LOCAL_PATH = LOCAL_ROOT_PATH + 'main/templates/' + APP_PREFIX_NAME + '/'
 
 DEBUG = True
 
+SITE_ROOT = "/tmp"
 # Email settings
 #EMAIL_USE_TLS = True
 #EMAIL_HOST = 'mail.gandi.net'
