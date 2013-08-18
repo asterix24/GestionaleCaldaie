c750e0a2e641f9ac5d396d0880eadd0899fe51d5
Daniele Basile <asterix@develer.com>
2013-08-18 15:37:04 +0000

    Add custom context dictionary.

--- gestionale/__init__.py
+++ gestionale/__init__.py
@@ -2,6 +2,8 @@ import os
 if os.environ["DJANGO_SETTINGS_MODULE"] == "gestionale.besalba.settings":
     from besalba import local_settings
     from besalba import settings
+    from demo.local_settings import local_env
 if os.environ["DJANGO_SETTINGS_MODULE"] == "gestionale.demo.settings":
     from demo import local_settings
     from demo import settings
+    from demo.local_settings import local_env
