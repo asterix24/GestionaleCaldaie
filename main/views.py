d4857bf29bc11d902c3103bfc7aa31cde8c22149
Daniele Basile <asterix24@gmail.com>
2013-11-24 23:14:53 +0100

    Move in separate module.

--- main/views.py
+++ main/views.py
@@ -169,56 +169,6 @@ def test(request):
     #print show, hide
     return render(request, 'test.sub', {'items_show': show, 'items_hide':hide })
 
-from main.models import Cliente
-
-def __settings_ShowHide(settings, key, default, idx=0):
-    settings = settings.values()
-    show = default
-    hide = []
-    if idx == len(settings) and idx > 0:
-        idx = idx - 1
-
-    if settings:
-        settings = settings[idx]
-        s = settings.get(key, '')
-        if s:
-            show = s.split('-')
-        else:
-            logger.error("No custom settings for %s, foldback on default.", key)
-    else:
-        logger.error("No custom settings for %s, foldback on default.", key)
-
-    for i in cfg.CFG_ALL:
-        if i in show:
-            continue
-        hide.append(i)
-
-    return show, hide
-
-
-from django.core.exceptions import ObjectDoesNotExist
-
-def slide_list(request):
-    home_view_show = []
-    home_view_hide = []
-
-    # get settings from files
-    if request.method == "GET":
-        settings = models.Settings.objects.all()
-        home_view_show, home_view_hide = __settings_ShowHide(settings, 'home_view', cfg.HOME_STD_VIEW)
-
-    if request.method == "POST":
-        try:
-            select = models.Settings.objects.get(pk=0)
-        except ObjectDoesNotExist:
-            select = None
-
-        form = models.SettingsForm(request.POST, instance=select)
-        if form.is_valid():
-            form.save()
-
-    return render(request, "test.sub", { 'home_show':home_view_show, 'home_hide':home_view_hide})
-
 
 from django.http import HttpResponse
 from functools import partial
